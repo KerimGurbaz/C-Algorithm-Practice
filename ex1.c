8fa1c36d88b8c9c9622a45969f859e819c213e01
KerimGurbaz <kerim.grbz.01@gmail.com>
2026-02-21 23:56:40 +0100

    structures-controle

--- ex1.c
+++ ex1.c
@@ -25,19 +25,63 @@ int main() {
 
     // return 0;
 
-    int x, y, z;
-    scanf("%d%d%d", &x, &y, &z);
+    // int x, y, z;
+    // scanf("%d%d%d", &x, &y, &z);
 
-    if (x > y) {
-        if (x > z) {
-            printf("Le maximum est : %d\n", x);
-        } else {
-            printf("Le maximum est : %d\n", z);
-        }
-    } else if (y > z) {
-        printf("Le maximum est : %d\n", y);
-    } else {
-        printf("Le maximum est : %d\n", z);
-    }
+    // if (x > y) {
+    //     if (x > z) {
+    //         printf("Le maximum est : %d\n", x);
+    //     } else {
+    //         printf("Le maximum est : %d\n", z);
+    //     }
+    // } else if (y > z) {
+    //     printf("Le maximum est : %d\n", y);
+    // } else {
+    //     printf("Le maximum est : %d\n", z);
+    // }
+
+
+    // int num;
+    // printf("entre un numbre");
+    // scanf("%d", &num);
+
+    // switch (num) {
+    // case 1:printf("Lundi");
+    //     break;
+    // case 2:printf("Mardi");
+    //     break;
+    // case 3:printf("Mecredi");
+    //     break;
+    // case 4:printf("Jeudi");
+    //     break;
+    // case 5:printf("Vendredi");
+    //     break;
+    // case 6:printf("Samedi");
+    //     break;
+    // case 7:printf("Dimance");
+    //     break;
+    // default: printf("Error");
+    // }
+
+    // int day;
+    // const char *days[] = { "Lundi", "Mardi", "Mercredi",
+    //     "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+
+    // printf("Saisir le numero du jour (1-7)");
+    // scanf("%d", &day);
+
+    // if (day >= 1 && day <= 7) {
+    //     printf("%s\n", days[day - 1]);
+    // } else {
+    //     printf("Erreur: numero invalide.\n");
+    // }
+
+    // return 0;
+
+    double x, y;
+
+    char op;
+
+
 
 }
