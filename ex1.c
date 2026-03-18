b4e35efdb32cbe6a42976d41996b96261303ac5c
KerimGurbaz <kerim.grbz.01@gmail.com>
2026-03-18 14:07:41 +0100

    v2

--- ex1.c
+++ ex1.c
@@ -339,46 +339,425 @@
 
 //     return 0;
 // }
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <limits.h>
+// int indece(const int *t, int n) {
+//     int min_val = INT_MAX;
+//     int idx = 0;
+
+//     for (int i = 0; i < n; ++i) {
+//         if (t[i] < min_val) {
+//             idx = i;
+//             min_val = t[i];
+//         }
+//     }
+//     return idx;
+// }
+
+// int main() {
+//     int n;
+
+
+//     if (scanf("%d", &n) != 1 || n <= 0) return 1;
+
+//     int *tab = malloc(n * sizeof * tab);
+//     if (tab == NULL) {
+//         return 1;
+//     }
+
+//     for (int i = 0; i < n; ++i) {
+//         if (scanf("%d", &tab[i]) != 1) {
+//             free(tab);
+//             return 1;
+//         }
+//     }
+
+//     int idx = indece(tab, n);
+
+//     printf("\nMin idx = %d et valeur : %d", idx, tab[idx]);
+
+
+//     free(tab);
+
+//     return 0;
+// }
+
+////////////////////////////////////////////////////////////////////
+
+//soru 1
 #include <stdlib.h>
 #include <stdio.h>
-#include <limits.h>
-int indece(const int *t, int n) {
-    int min_val = INT_MAX;
-    int idx = 0;
 
-    for (int i = 0; i < n; ++i) {
-        if (t[i] < min_val) {
-            idx = i;
-            min_val = t[i];
-        }
-    }
-    return idx;
-}
+
+// int main() {
+//     int n;
+
+//     if (scanf("%d", &n) != 1 || n <= 0) return 1;
+
+//     int *tab = malloc(n * sizeof * tab);
+//     if (tab == NULL) {
+//         return 1;
+//     }
+
+//     for (int i = 0; i < n; ++i) {
+//         if (scanf("%d", &tab[i]) != 1) {
+//             free(tab);
+//             return 1;
+//         }
+//     }
+
+//     int *temp = realloc(tab, (2 * n) * sizeof * temp);
+//     if (temp == NULL) {
+//         free(tab);
+//         return 1;
+//     }
+//     tab = temp;
+
+//     for (int i = n; i < 2 * n; ++i) {
+//         if (scanf("%d", &tab[i]) != 1) {
+//             free(tab);
+//             return 1;
+//         }
+//     }
+//     printf("\n");
+//     for (int i = 0; i < 2 * n; ++i) {
+//         printf("%d", tab[i]);
+//     }
+
+//     printf("\n");
+
+//     free(tab);
+
+
+//     return 0;
+// }
+
+
+// int main() {
+//     int n;
+
+//     if (scanf("%d", &n) != 1 || n <= 0) return 1;
+
+//     int *tab = malloc(n * sizeof * tab);
+//     if (tab == NULL) {
+//         return 1;
+//     }
+
+//     for (int i = 0; i < n; ++i) {
+//         if (scanf("%d", &tab[i]) != 1) {
+//             free(tab);
+//             return 1;
+//         }
+//     }
+
+//     int *temp = realloc(tab, (n + 1) * sizeof * temp);
+//     if (temp == NULL) {
+//         free(tab);
+//         return 1;
+//     }
+//     tab = temp;
+
+//     for (int i = n; i < n + 1; ++i) {
+//         if (scanf("%d", &tab[i]) != 1) {
+//             free(tab);
+//             return 1;
+//         }
+//     }
+//     printf("\n");
+//     for (int i = 0; i < n + 1; ++i) {
+//         printf("%d ", tab[i]);
+//     }
+
+//     printf("\n");
+
+//     free(tab);
+
+
+//     return 0;
+// }
+
+// int main() {
+//     int n;
+
+//     if (scanf("%d", &n) != 1 || n <= 0) return 1;
+
+//     int *tab = malloc(n * sizeof * tab);
+//     if (tab == NULL) {
+//         return 1;
+//     }
+
+//     for (int i = 0; i < n; ++i) {
+//         if (scanf("%d", &tab[i]) != 1) {
+//             free(tab);
+//             return 1;
+//         }
+//     }
+
+//     int *temp = realloc(tab, (n - 1) * sizeof * temp);
+//     if (temp == NULL) {
+//         free(tab);
+//         return 1;
+//     }
+//     tab = temp;
+
+
+//     printf("\n");
+//     for (int i = 0; i < n - 1; ++i) {
+//         printf("%d ", tab[i]);
+//     }
+
+//     printf("\n");
+
+//     free(tab);
+
+
+//     return 0;
+// }
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main() {
+//     int n;
+//     printf("Entrez la taille : \n");
+//     if (scanf("%d", &n) != 1 || n <= 1) return 1;
+
+//     int *tab = malloc(n * sizeof * tab);
+//     if (!tab) return 1;
+
+//     for (int i = 0; i < n; ++i) scanf("%d", tab + i);
+//     int *ptr_des = tab;
+//     int *ptr_src = tab + 1;
+
+//     for (int i = 0; i < n - 1; ++i) {
+//         *ptr_des = *ptr_src;
+//         ++ptr_des;
+//         ++ptr_src;
+//     }
+//     int *temp = realloc(tab, (n - 1) * sizeof * temp);
+//     if (temp != NULL) {
+//         tab = temp;
+//     }
+
+//     printf("Resultat : ");
+//     for (size_t i = 0; i < n - 1; i++) printf("%d ", *(tab + i));
+//     printf("\n");
+
+//     free(tab);
+//     return 0;
+
+
+//     return 0;
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CAPACITY 1
+
+
+// int main() {
+//     int taille = 0;
+//     int valeur;
+
+//     int *tab = malloc(CAPACITY * sizeof * tab);
+//     if (tab == NULL) return 1;
+//     printf("Entrée: \n");
+//     do {
+//         if (scanf("%d", &valeur) != 1) {
+//             free(tab);
+//             return 1;
+//         };
+//         if (valeur != 0) {
+
+//             tab[taille++] = valeur;
+//         }
+
+//         int *temp = realloc(tab, (CAPACITY + 1) * sizeof * temp);
+//         if (temp == NULL) {
+//             free(tab);
+//             return 1;
+//         }
+//         tab = temp;
+
+//     }
+//     while (valeur != 0);
+
+
+//     printf("Sortie:\n %d valeurs ", taille);
+//     for (int i = 0; i < taille; ++i) {
+//         printf("%d ", tab[i]);
+//     }
+
+
+//     free(tab);
+
+
+//     return 0;
+// }
+
+
+// int main() {
+//     int taille = 0;
+//     int valeur;
+
+//     int *tab = malloc(1 * sizeof * tab);
+//     if (tab == NULL) return 1;
+
+
+//     printf("Entree: \n");
+
+//     while (1) {
+//         if (scanf("%d", &valeur) != 1) {
+//             free(tab);
+//             return 1;
+//         }
+
+//         if (valeur == 0) break;
+//         tab[taille++] = valeur;
+//     }
+
+//     int *temp = realloc(tab, (taille + 1) * sizeof * temp);
+
+//     if (temp == NULL) {
+//         free(tab);
+//         return 1;
+//     }
+//     tab = temp;
+
+//     printf("Sortie:\n%d valeurs\n", taille);
+//     for (int i = 0; i < taille; ++i) {
+//         printf("%d ", tab[i]);
+//     }
+//     printf("\n");
+
+//     free(tab);
+//     return 0;
+// }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// int main() {
+//     int taille = 0;
+//     int capacity = 1;
+//     int valeur;
+
+//     int *tab = malloc(capacity * sizeof * tab);
+//     if (tab == NULL) return 1;
+
+//     while (1) {
+
+//         if (scanf("%d", &valeur) != 1) {
+//             free(tab);
+//             return 1;
+//         }
+
+//         if (valeur == 0) break;
+//         if (capacity == taille) {
+//             capacity *= 2;
+//             int *temp = realloc(tab, capacity * sizeof * temp);
+//             if (temp == NULL) {
+//                 free(tab);
+//                 return 1;
+//             }
+//             tab = temp;
+//         }
+//         tab[taille++] = valeur;
+//         printf("Capacity : %d\n", capacity);
+//     }
+//     printf("\n");
+
+//     for (int i = 0; i < taille; ++i) {
+//         printf("%d ", tab[i]);
+//     }
+//     free(tab);
+//     return 0;
+// }
+
+// void ajouter_valeur(int **t, int *taille, int *cap, int valeur) {
+//     if (*taille == *cap) {
+//         *cap *= 2;
+
+//         int *temp = realloc(*t, (*cap) * sizeof * temp);
+//         if (temp != NULL) {
+//             *t = temp;
+//         } else {
+//             free(*t);
+//             return 1;
+//         }
+//     }
+
+//     (*t)[*taille] = valeur;
+//     (*taille)++;
+// }
+
+// int main() {
+//     int taille = 0;
+//     int capacity = 1;
+//     int *tab = malloc(capacity * sizeof * tab);
+//     if (tab == NULL) return 1;
+
+//     int valeur;
+//     while (scanf("%d", &valeur) == 1 && valeur != 0) {
+
+//         ajouter_valeur(&tab, &taille, &capacity, valeur);
+//     }
+
+//     for (int i = 0; i < taille; ++i) {
+//         printf("%d ", tab[i]);
+//     }
+
+
+//     free(tab);
+//     return 0;
+// }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TAILLE 5
+
 
 int main() {
-    int n;
+    size_t lignes = 5;
+    size_t colonnes = 10;
+    int **tab;
 
+    tab = malloc(lignes * sizeof(int *));
+    if (tab == NULL) return 1;
 
-    if (scanf("%d", &n) != 1 || n <= 0) return 1;
-
-    int *tab = malloc(n * sizeof * tab);
-    if (tab == NULL) {
-        return 1;
-    }
-
-    for (int i = 0; i < n; ++i) {
-        if (scanf("%d", &tab[i]) != 1) {
+    for (int i = 0; i < lignes; ++i) {
+        tab[i] = malloc(colonnes * sizeof(int));
+        if (tab[i] == NULL) {
+            for (size_t j = 0; j < i; j++) {
+                free(tab[j]);
+            }
             free(tab);
             return 1;
         }
     }
 
-    int idx = indece(tab, n);
+    for (size_t i = 0; i < lignes; ++i) {
+        for (size_t j = 0; j < colonnes; ++j) {
+            tab[i][j] = (i + 1) * (j + 1);
+        }
+    }
 
-    printf("\nMin idx = %d et valeur : %d", idx, tab[idx]);
+    for (size_t i = 0; i < lignes; ++i) {
+        for (size_t j = 0; j < colonnes; ++j) {
+            printf("%4d", tab[i][j]);
+        }
+        printf("\n");
+    }
 
-
+    for (size_t i = 0; i < lignes; ++i) {
+        free(tab[i]);
+    }
     free(tab);
 
     return 0;
+
 }
