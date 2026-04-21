b032a8f28aad3a123d5d327d25e57da80e6825bc
KerimGurbaz <kerim.grbz.01@gmail.com>
2026-04-21 12:02:15 +0200

    structs...

--- ex1.c
+++ ex1.c
@@ -1,391 +1,203 @@
 #include <stdio.h>
-#include <stdint.h>
 
-// #define LECTURE (1<<0)
-// #define ECRITURE (1<<1)
-// #define EXECUTION (1<<2)
-// #define ADMIN (1<<3)
-
-
-// void afficher_binaire(uint8_t octet) {
-//     for (int i = 7; i >= 0; --i) {
-//         printf("%d", (octet >> i) & 1);
-//     }
-//     printf("\n");
-// }
+// typedef struct {
+//     float x;
+//     float y;
+// }Point2D;
 
 // int main() {
 
+//     Point2D p1;
+//     p1.x = 5.5;
+//     p1.y = 10.2;
 
-//     uint8_t droits = 0;
-
-//     printf("---Gestion DES Droit d'acces--\n\n");
-
-//     droits = LECTURE | ECRITURE;
-//     printf("1. ajout Lectre et Ecriture [L | E] : ");
-//     afficher_binaire(droits);
-
-//     droits |= EXECUTION;
-//     printf("2. Ajout Execution (L|E|X)  : ");
-//     afficher_binaire(droits);
-
-//     printf("\n--VERIFICATION--\n");
-//     if (droits & ECRITURE) {
-//         printf("[OK] le droit d'ECRITURE est present.\n");
-//     }
-//     if (!(droits & ADMIN)) {
-//         printf("[NO] le lecture d'ADMIN n'est pas present.\n");
-//     }
-
-
-//     droits &= ~ECRITURE;
-//     printf("\n3. Retrait de l'ecriture (L|X)    : ");
-//     afficher_binaire(droits);
+//     printf("x = %.2f && y= %.2f\n", p1.x, p1.y);
 
 //     return 0;
 // }
 
 
+// typedef struct {
+//     float x;
+//     float y;
+// }Point2D;
 
-// #define LECTURE (1<<0)
-// #define ECRITURE (1<<1)
-// #define EXECUTION (1<<2)
-// #define ADMIN (1<<3)
-
-
-// void afficher_binaire(uint8_t octet) {
-//     for (int i = 7; i >= 0; --i) {
-//         printf("%d", (octet >> i) & 1);
-//     }
-//     printf("\n");
-// }
+// void deplacer(Point2D *p, float dx, float dy) {
+//     if (p == NULL)return;
+//     p->x += dx;
+//     p->y += dy;
+// };
 
 // int main() {
-//     uint8_t droits = 0;
-//     printf("---Gestion DES Droit d'acces--\n\n");
+//     Point2D p1;
+//     p1.x = 5.5;
+//     p1.y = 10.2;
 
-//     droits = LECTURE | ECRITURE;
+//     deplacer(&p1, 1.2, 2.2);
 
-//     printf("1. ajout Lectre et Ecriture [L | E] : ");
-//     afficher_binaire(droits);
-
-//     droits |= EXECUTION;
-//     printf("2.Ajout EXECUTION : ");
-//     afficher_binaire(droits);
-
-//     printf("\n--- VERIFICATION ---\n");
-
-//     if (droits & ECRITURE) {
-//         printf("ok\n");
-//     }
-//     if (!(droits & ADMIN)) {
-//         printf("no\n");
-//     }
-
-//     droits &= ~ECRITURE;
-//     printf("Retrait de l'ecriture : ");
-//     afficher_binaire(droits);
+//     printf("x = %.1f && y= %.1f\n", p1.x, p1.y);
 
 //     return 0;
-
 // }
 
-// #include <stdio.h>
-// #include <stdint.h>
+// typedef struct {
+//     int id;
+//     float valeur;
+// }Capteur;
 
-// #define PERM_EXEC 0x01
-// #define PERM_WRITE 0x02
-// #define PERM_READ 0x04
-
-// //Foction utilitaire pour verifier l'etat des bits
-
-// void affciher_droits(uint8_t droits) {
-//     printf("Lecture : %s\n", (droits & PERM_READ) ? "[X] AUTORISE" : "[ ] REFUSE");
-//     printf("Ecriture :%s\n", (droits & PERM_WRITE) ? "[X] AUTORISE" : "[ ] REFUSE");
-//     printf("Execution : %s\n", (droits & PERM_EXEC) ? "[X] AUTORISE" : "[ ] REFUSE");
-//     printf("-> valeur memoire : %d (Hex: 0x%02X)\n\n", droits, droits);
-// }
 
 // int main() {
-//     uint8_t droits = 0x00;
-//     printf("ETAPE 1: Fichier nouvellement cree---\n");
-//     affciher_droits(droits);
-
-//     printf("ETAPE 2: Ajout du droit de LECTURE\n");
-//     droits |= PERM_READ;
-//     affciher_droits(droits);
-
-//     printf("ETAPE 3 : Ajout ECRITURE et EXECUTION d'un coup\n");
-//     droits |= (PERM_WRITE | PERM_EXEC);
-//     affciher_droits(droits);
-
-//     printf("ETAPE 4 : Retrait du droit d'ecriture\n");
-//     droits &= ~PERM_WRITE;
-//     affciher_droits(droits);
+//     Capteur cap[3] = {
+//         {1, 24.5},
+//         {2, 34.5},
+//         {3, 44.5}
+//     };
 
 
-//     return 0;
-
-// }
-
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <stdint.h>
-// #include <time.h>
-// #include <unistd.h>
-
-// #define CLIGNOTEMENTS 20
-
-// void afficher(uint8_t etat) {
-//     printf("\r");
-
-//     for (int i = 7; i >= 0; --i) {
-//         if ((etat >> i) & 1) {
-//             printf("*");
-//         } else {
-//             printf("-");
+//     for (int i = 0; i < 3; ++i) {
+//         if (cap[i].valeur > 25) {
+//             printf("ID: %d = %.1f\n", i, cap[i].valeur);
 //         }
 //     }
 
-//     fflush(stdout);
+//     return 0;
 // }
+
+
+// typedef struct {
+//     int jour;
+//     int mois;
+//     int annee;
+// }Date;
+
+// typedef struct {
+//     int id;
+//     float prix;
+//     Date date_expiration;
+// }Produit;
+
 // int main() {
-//     srand(time(NULL));
+//     Produit p;
+//     p.id = 1;
+//     p.prix = 12.99;
+//     p.date_expiration.jour = 15;
+//     p.date_expiration.mois = 8;
+//     p.date_expiration.annee = 2027;
 
-//     uint8_t guirlande = 0xAA;
+//     Produit p2 = { 2, 45.50, {15,9,2030} };
 
-//     printf("simulation de la guirlande en cours...\n");
+//     printf("Produit ID :%d | Prix:%.2f | EXpiration : %02d/%02d/%d\n",
+//         p.id,
+//         p.prix,
+//         p.date_expiration.jour,
+//         p.date_expiration.mois,
+//         p.date_expiration.annee
+//     );
+//     return 0;
+// }
+// #include <string.h>
 
-//     for (int i = 0; i < CLIGNOTEMENTS; ++i) {
-//         afficher(guirlande);
+// typedef struct {
+//     char nom[50];
+//     float notes[5];
 
-//         int delai = (rand() % 900001) + 100000;
-//         usleep(delai);
+// }Etudiant;
 
-//         guirlande = ~guirlande;
+// void afficher(const Etudiant *e) {
+//     if (e == NULL) return;
+
+//     float somme = 0.0;
+
+//     printf("---Bulletin de %s ---", e->nom);
+//     for (int i = 0; i < 5; ++i) {
+//         printf("Note %d : %.2f\n", i + 1, e->notes[i]);
+//         somme += e->notes[i];
 //     }
-//     printf("\nTermine.\n");
+//     printf(">> Moyenne : %.2f\n\n", somme / 5.0);
+
+
+// }
+
+// int main() {
+//     Etudiant e1;
+
+//     strncpy(e1.nom, "Kerim GURBAZ", sizeof(e1.nom) - 1);
+//     e1.nom[sizeof(e1.nom) - 1] = '\0';
+
+//     e1.notes[0] = 14.5;
+//     e1.notes[1] = 16.0;
+//     e1.notes[2] = 12.5;
+//     e1.notes[3] = 18.0;
+//     e1.notes[4] = 15.0;
+
+//     afficher(&e1);
 
 //     return 0;
 // }
 
 // #include <stdio.h>
 // #include <stdlib.h>
-// #include <stdint.h>
-// #include <unistd.h>
-// #include <time.h>
+// #include <string.h>
 
-// #define CLIOGNOTEMENTS 20
+// #define MAX_MARQUE 50
 
-// void afficher_guirlande(uint8_t etat) {
-//     printf("\r");
+// typedef struct {
+//     char marque[MAX_MARQUE];
+//     int kilomertrage;
+// }Vehicule;
 
-//     for (int i = 7; i >= 0; --i) {
-//         if ((etat >> i) & 1) {
-//             printf("*");
-//         } else {
-//             printf("-");
-//         }
-//     }
-//     fflush(stdout);
-// }
+// Vehicule *cree_vehicule(const char *marque, int km) {
+//     Vehicule *nouveau_vehicule = malloc(sizeof(Vehicule));
 
-// int main() {
-//     srand(time(NULL));
-//     uint8_t guirlande = 0xAA;
-
-//     printf("Simulation de la guirlande (%d clignotements) : \n\n", CLIOGNOTEMENTS);
-
-//     for (int i = 0; i < CLIOGNOTEMENTS; ++i) {
-//         afficher_guirlande(guirlande);
-
-//         int min_us = 100000;
-//         int max_us = 1000000;
-//         int delai = min_us + (rand() % (max_us - min_us + 1));
-
-//         usleep(delai);
-
-//         guirlande = ~guirlande;
-//     }
-//     printf("\n\nJoyeuses FETES!\n");
-
-//     return 0;
-// }
-
-
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <stdint.h>
-
-
-// int main() {
-
-//     uint8_t device = 0b01001000;
-
-//     if (device & (1 << 3)) {
-//         printf("ok");
-//     } else {
-//         printf("non");
+//     if (nouveau_vehicule == NULL) {
+//         printf("[!]Erreur d'allocation memoire.\n");
+//         return NULL;
 //     }
 
-//     return 0;
-// }
+//     strncpy(nouveau_vehicule->marque, marque, MAX_MARQUE - 1);
+//     nouveau_vehicule->marque[MAX_MARQUE - 1] = '\0';
+//     nouveau_vehicule->kilomertrage = km;
 
-
-
-// void activate_bit(uint8_t *val, int bit_index) {
-//     if (val == NULL) return;
-//     *val |= (1 << bit_index);
+//     return nouveau_vehicule;
 // }
 
 // int main() {
-//     uint8_t status = 0;
+//     Vehicule *mon_auto = cree_vehicule("Porsche 911", 45000);
 
-//     printf("Dabord : %u\n", status);
+//     if (mon_auto != NULL) {
 
-//     activate_bit(&status, 3);
-//     printf("3. bit actif : %u\n", status);
+//         printf("--- Vehicule Enregistre -- \n");
+//         printf("MArque : %s\n", mon_auto->marque);
+//         printf("KM : %d\n", mon_auto->kilomertrage);
 
-//     activate_bit(&status, 7);
-//     printf("0. ve 3. bit actif : %u\n", status);
+//         free(mon_auto);
 
-
-//     return 0;
-// }
-
-// #include <stdint.h>
-// #include <stdio.h>
-// void afficher(uint8_t number) {
-//     for (int i = 7; i >= 0; --i) {
-//         printf("%d", (number >> i) & 1);
-//     }
-//     printf("\n");
-// }
-// int main() {
-//     int number;
-//     if (scanf("%d", &number) != 1) {
-//         return 1;
-//     }
-
-//     afficher(number);
-
-//     return 0;
-// }
-
-
-// #include <stdint.h>
-// #include <stdio.h>
-
-// #define ISIK 0x01
-// #define KLIMA 0x02
-// #define ALARM 0x04
-// #define KAPI_KILIDI 0x08
-
-// int main() {
-//     uint8_t etat = 0x00;
-//     printf("0x%02X\n", etat);
-
-//     etat |= (ISIK | ALARM);
-//     printf("0x%02X\n", etat);
-
-//     etat &= ~ISIK;
-//     printf("0x%02X\n", etat);
-
-//     printf("Klima is %s\n", (etat & KLIMA) ? "OPEN" : "CLOSE");
-
-//     return 0;
-// }
-
-
-// #include <stdint.h>
-// #include <stdio.h>
-// #include <unistd.h>
-
-// int main() {
-//     uint8_t pos = 1;
-
-//     printf("Yukleniyor animasyonu baslatiliyor...\n");
-
-//     for (int i = 0; i < 8; ++i) {
-//         printf("\r");
-
-//         for (int i = 7; i >= 0; --i) {
-//             if ((pos >> i) & 1) {
-//                 printf("*");
-//             } else {
-//                 printf("-");
-//             }
-//         }
-//         fflush(stdout);
-
-//         usleep(500000);
-//         pos <<= 1;
 //     }
 
 //     return 0;
 // }
 
 #include <stdio.h>
-#include <string.h>
+typedef struct {
+    char c1;
+    int i;
+    char c2;
 
-#define MAX_LIVRES 100
-#define MAX_TITRE 100
-#define MAX_AUTEUR 50
+}StructA;
 
 typedef struct {
-    char titre[MAX_TITRE];
-    char auteur[MAX_AUTEUR];
-    int annee_publication;
-    int nobre_copies;
-}Livre;
+    int i;
+    char c1;
+    char c2;
 
-int ajouter_livre(Livre inventaire[], int *nb_livres, const char *titre, const char *auteur, int annee, int copies) {
-    if (*nb_livres >= MAX_LIVRES) {
-        printf("Erreur : inventaire plein.\n");
-        return 0;
-    }
-
-    strncpy(inventaire[*nb_livres].titre, titre, MAX_TITRE - 1);
-    inventaire[*nb_livres].titre[MAX_TITRE - 1] = '\0';
-
-    strncpy(inventaire[*nb_livres].auteur, auteur, MAX_AUTEUR - 1);
-    inventaire[*nb_livres].auteur[MAX_AUTEUR - 1] = '\0';
-
-
-    inventaire[*nb_livres].annee_publication = annee;
-    inventaire[*nb_livres].nobre_copies = copies;
-
-    (*nb_livres)++;
-
-    return 1; //basarili;
-}
-
-void maj_stock(Livre *livre, int variation) {
-
-    if (livre == NULL) return;
-
-    if (livre->nobre_copies + variation < 0) {
-        printf("Erreur :  impossible d'emprunter '%s' , stock insuffisant(0 copie). ", livre->titre);
-    } else {
-        livre->nobre_copies += variation;
-        printf("Succes : Stock de '%s' mis a jour. Nouvel etat : %d copies.\n", livre->titre, livre->nobre_copies);
-    }
-}
-
+}StructB;
 
 int main() {
-    Livre bibliotheque[MAX_LIVRES];
-    int total_livres = 0;
+    printf("--- Test de Memoire (Memory Alignment) ---\n");
 
-    printf("--INITIALISATION DE L'INVENTAIRE---\n");
+    // Beklenti: 1 + 4 + 1 = 6 byte olması yönündedir. Ama sonuçlar şaşırtacak.
+    printf("Taille de Struct A : %zu octets\n", sizeof(StructA)); // Çıktı: 12
+    printf("Taille de Struct B : %zu octets\n", sizeof(StructB)); // Çıktı: 8
 
-    ajouter_livre(bibliotheque, &total_livres, "Le C en 20 heure", "DEnis Ritchie", 1972, 3);
-    ajouter_livre(bibliotheque, &total_livres, "Pointeurs Avances", "Jean Michel", 2021, 1);
-
-    printf("\n--- OPERATIONS SUR LE STOCK ---\n");
-
-    maj_stock(&bibliotheque[0], -1);
-
-    maj_stock(&bibliotheque[1], -1);
+    return 0;
 }
