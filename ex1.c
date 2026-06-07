d59f82abd5a5065a548eea977720fe06be0f10ae
KerimGurbaz <kerim.grbz.01@gmail.com>
2026-06-07 10:26:03 +0200

    prep exam.

--- ex1.c
+++ ex1.c
@@ -1,179 +1,137 @@
-// // #include <stdio.h>
-// // #include <stdlib.h>
-
-// // typedef struct Node {
-// //     int data;
-// //     struct Node *next;
-// // }Node;
-
-// // typedef struct Queue {
-// //     Node *head;
-// //     Node *tail;
-// // }Queue;
-
-
-// // void push_in_queue(Queue *q, int value) {
-
-// //     if (q == NULL)return;
-// //     Node *new_node = malloc(sizeof(Node));
-// //     if (new_node == NULL) {
-// //         return;
-// //     }
-
-// //     new_node->data = value;
-// //     new_node->next = NULL;
-
-// //     if (q->head == NULL) {
-// //         q->head = new_node;
-// //         q->tail = new_node;
-// //     } else {
-// //         q->tail->next = new_node;
-// //         q->tail = new_node;
-
-// //     }
-
-// // }
-
-// // int pop_from_queue(Queue *q) {
-// //     if (q->head == NULL) {
-// //         return -1;
-// //     }
-
-// //     Node *temp_node = q->head;
-// //     int pop_value = temp_node->data;
-
-// //     q->head = q->head->next;
-
-// //     if (q->head == NULL) {
-// //         q->tail = NULL;
-// //     }
-
-// //     free(temp_node);
-
-// //     return pop_value;
-
-// // }
-
-
-// // int main() {
-
-// //     Queue q;
-// //     q.head = NULL;
-// //     q.tail = NULL;
-
-// //     Queue q2 = { NULL, NULL };
-
-// //     push_in_queue(&q2, 5);
-
-
-// //     return 0;
-// // }
-
-
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include "queue.h"
+#include <stdio.h>
 
 
 
-// void init_queue(Queue *q) {
-//     if (q != NULL) {
-//         q->head = NULL;
-//         q->tail = NULL;
-//         q->size = 0;
-//     }
-// }
-
-// void push_in_queue(Queue *q, int value) {
-//     if (q == NULL) return;
-
-//     Node *new_node = malloc(sizeof(Node));
-
-//     if (new_node == NULL) {
-//         fprintf(stderr, "Erreur d'allocation memoire.\n");
-//         exit(EXIT_FAILURE);
-//     }
-
-//     new_node->value = value;
-//     new_node->next = NULL;
-
-//     if (q->tail == NULL) {
-//         q->head = new_node;
-//         q->tail = new_node;
-//     } else {
-//         q->tail->next = new_node;
-//         q->tail = new_node;
-//     }
-//     q->size++;
-// }
-
-// int pop_from_queue(Queue *q) {
-//     if (q == NULL || q->head == NULL) {
-//         fprintf(stderr, "Erreur...");
-//         exit(EXIT_FAILURE);
-//     }
-//     Node *temp = q->head;
-//     int pop_value = temp->value;
-
-//     q->head = temp->next;
-
-//     if (q->head == NULL) {
-//         q->tail = NULL;
-//     }
-
-//     free(temp);
-//     q->size--;
-
-//     return pop_value;
-// }
-
-
-// int is_empty(Queue *q) {
-//     return (q == NULL || q->head == NULL);
-// }
-
-
-
-// int  main() {
-//     Queue q;
-
-//     init_queue(&q);
-
-//     for (int i = 0; i < 10; ++i) {
-//         printf("%d ", push_in_queue(&q));
-//     }
-
-//     for (int i = 0; i < 5; ++i) {
-//         printf("%d ", pop_from_queue(&q));
-//     }
+// void fibo(int n);
+// int main() {
+//     int count = 0;
+//     int n;
+//     printf("Entrez un value\n");
+//     if (scanf("%d", &n) != 1) return 1;
+//     fibo(n);
 //     printf("\n");
 
-//     if (!is_empty(&q)) {
-//         printf("%d elements remain in queue, from %d to %d.\n",
-//             q.size,
-//             q.head->value,
-//             q.tail->value);
-//     }
 //     return 0;
 // }
-#include <stdio.h>
-#include "queue.h"
+
+// void fibo(int n) {
+//     int premier = 0;
+//     int seconde = 1;
+
+//     if (n >= 1) printf("%d ", premier);
+//     if (n >= 2)printf("%d ", seconde);
+
+//     for (int i = 3; i <= n; ++i) {
+//         int suivant = premier + seconde;
+//         printf("%d ", suivant);
+//         premier = seconde;
+//         seconde = suivant;
+//     }
+// }
+
+// void trouver_min_max(const int tab[], int size, int *mn, int *mx) {
+//     for (int i = 1; i < size;++i) {
+//         if (tab[i] > *mx) {
+//             *mx = tab[i];
+//         }
+//         if (tab[i] < *mn) {
+//             *mn = tab[i];
+//         }
+//     }
+// }
+
+
+// int main() {
+
+//     int tab[] = { 12, 5, 8, 19, 1, 7 };
+//     int size = sizeof(tab) / sizeof(tab[0]);
+//     int minVal = tab[0];
+//     int maxVal = tab[0];
+
+//     trouver_min_max(tab, size, &minVal, &maxVal);
+//     printf("min_value = %d \n", minVal);
+//     printf("max_value = %d \n", maxVal);
+
+//     return 0;
+// }
+
+// void supprimer_espaces(char *str) {
+//     char *p_left = str;
+//     char *p_right = str;
+
+//     while (*p_right != '\0') {
+
+//         if (*p_right != ' ') {
+//             *p_left = *p_right;
+//             p_left++;
+//             p_right++;
+//         } else {
+//             p_right++;
+//         }
+
+//     }
+//     *p_left = '\0';
+
+// }
+
+// int main() {
+//     char str[] = "Programmation en C";
+//     printf("%s\n", str);
+//     supprimer_espaces(str);
+//     printf("%s\n", str);
+//     return 0;
+// }
+
+#include <stdlib.h>
 
 int main() {
-    void *q = new_queue();
+    int n = 0;
+    int capacty = 2;
+    int valeur;
 
-    for (int i = 0; i < 10; ++i) {
-        push_in_queue(q, i * i);
-        if (i % 2 == 0) {
-            printf("%d ", front_of_queue(q));
-            pop_from_queue(q);
+    int *tab = malloc(capacty * sizeof(int));
+    if (tab == NULL) {
+        fprintf(stderr, "Erreur allocation memoire");
+        return 1;
+    }
+    printf("Entrez des nombres positifs (négatif pour arrêter):\n");
+    while (1) {
+        printf("  Nombre %d: ", n + 1);
+        if (scanf("%d", &valeur) != 1) {
+            printf("Entrée invalide, ignorée.\n");
+            while (getchar() != '\n');
+            continue;
         }
+        if (valeur < 0) {
+            printf("Nombre négatif détecté, arrêt de la saisie.\n");
+            break;
+        }
+
+        if (n >= capacty) {
+            capacty *= 2;
+            int *temp = realloc(tab, capacty * sizeof(int));
+            if (temp == NULL) {
+                fprintf(stderr, "Erreur de reallocation memoire\n");
+                free(tab);
+                return 1;
+            }
+            tab = temp;
+        }
+        tab[n++] = valeur;
     }
 
-    printf("\n%d elements remain in queue, from %d to %d.\n",
-        size_of_queue(q),
-        front_of_queue(q),
-        back_of_queue(q));
+    printf("\n=== RÉSULTATS ===\n");
+    printf("Nombre d'éléments: %d\n", n);
+    printf("Capacité finale: %d\n", capacty);
+    printf("Contenu du tableau: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
 
-    free_queue(q);
+    free(tab);
+
+
+
+    return 0;
 }
